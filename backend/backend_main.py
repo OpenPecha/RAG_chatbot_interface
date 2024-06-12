274ad6029b2913feca28e47980b04f49303260a6
Tenzin <tenzintsunduebhattu@gmail.com>
2024-06-12 15:18:38 +0530

    code refactor

--- backend/backend_main.py
+++ backend/backend_main.py
@@ -1,9 +1,11 @@
-from typing import Union
+import os 
 from fastapi import FastAPI
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
+from llama_index.core import PromptTemplate
 
-
+from config import load_vector_db
+from fetch_response import get_chatgpt_response
 class UserInput(BaseModel):
     user_input: str
 
@@ -34,49 +36,12 @@ async def respond_to_user_input(user_input: UserInput):
     return {"response": response}
 
 
-
-#####################################################################################
-#                     LOAD VECTOR DATABASE                     #
-#####################################################################################
-
-import os 
-import chromadb
-from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.core import StorageContext, load_index_from_storage, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core import PromptTemplate
-
-from config import db_path, collection_name, persist_dir, EMBEDDING_MODEL
-
-
-def load_vector_db():
-    embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL,trust_remote_code=True)
-    Settings.embed_model = embed_model
-
-    """ Initialize the Chroma persistent client """
-    chroma_client = chromadb.PersistentClient(path=db_path)
-
-    """ Get or create the specified collection"""
-    chroma_collection = chroma_client.get_or_create_collection(collection_name)
-
-    """ Initialize the vector store with the specified collection """
-    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-
-    """ Initialize the storage context with the vector store and persist directory """
-    storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir=persist_dir)
-
-    """ Load the index from storage """
-    VECTOR_INDEX = load_index_from_storage(storage_context)
-    return VECTOR_INDEX
-
-
 def generate_answer(question, num_of_context=5)->str:
     question = question.replace('"',"'")
     retriever = VECTOR_INDEX.as_retriever(similarity_top_k=num_of_context)
     retrieved_nodes = retriever.retrieve(question)
 
     context = ""
-
     
     for retrieved_node in retrieved_nodes:
         context += f"{retrieved_node.get_content()} \n\n"
@@ -110,22 +75,5 @@ def generate_answer(question, num_of_context=5)->str:
 
 
 
-import requests
 
-def get_chatgpt_response(api_key, prompt):
-    url = "https://api.openai.com/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
-    data = {
-        "model": "gpt-4-turbo", 
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ]
-    }
-    response = requests.post(url, headers=headers, json=data)
-    response_json = response.json()
-    return response_json
 
--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,31 @@
-TEXT_GENERATION_ARGS = {
-        "max_new_tokens": 3000,
-        "return_full_text": False,
-        "temperature": 0,
-        "do_sample": False,
-    }
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext, load_index_from_storage, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
 EMBEDDING_MODEL = "Alibaba-NLP/gte-large-en-v1.5"
 
-
 db_path="./chroma_db"
 collection_name="rag_demo"
 persist_dir="./chroma_db/index"
+
+def load_vector_db():
+    embed_model = HuggingFaceEmbedding(model_name=EMBEDDING_MODEL,trust_remote_code=True)
+    Settings.embed_model = embed_model
+
+    """ Initialize the Chroma persistent client """
+    chroma_client = chromadb.PersistentClient(path=db_path)
+
+    """ Get or create the specified collection"""
+    chroma_collection = chroma_client.get_or_create_collection(collection_name)
+
+    """ Initialize the vector store with the specified collection """
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    """ Initialize the storage context with the vector store and persist directory """
+    storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir=persist_dir)
+
+    """ Load the index from storage """
+    VECTOR_INDEX = load_index_from_storage(storage_context)
+    return VECTOR_INDEX

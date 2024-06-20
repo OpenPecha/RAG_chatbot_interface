66d0a891159393afb4e29d96dc528f4e12e13048
Tenzin <tenzintsunduebhattu@gmail.com>
2024-06-20 11:41:09 +0530

    log answer and referene from llm response separate

--- backend/log_response.py
+++ backend/log_response.py
@@ -2,12 +2,18 @@ from pathlib import Path
 
 LOG_FILE_PATH = Path("rag_chatbot_log.txt")
 
-def log_rag_chatbot_response(question:str, answer:str):
+def log_rag_chatbot_response(question:str, response:str):
     if not LOG_FILE_PATH.exists():
         LOG_FILE_PATH.touch()
 
 
     question = question.replace("\n","")
-    answer = answer.replace("\n","")
+    response = response.replace("\n","")
+
+    """ response contains both answer and references"""
+    """ we will store them separelty in log file"""
+    response_parts = response.split("__References__")
+    answer, references = response_parts[0], response_parts[1]
+
     with open(LOG_FILE_PATH, "a", encoding="utf-8") as file:
-        file.write(f"Question: {question},Answer:{answer}\n")
+        file.write(f"Question: {question},Answer:{answer},References:{references}\n")

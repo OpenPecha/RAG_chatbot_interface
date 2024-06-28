cf3f27ccbfb56b9a65ea276eb73c28ce690481ae
Tenzin <tenzintsunduebhattu@gmail.com>
2024-06-28 15:24:40 +0530

    transform query function

--- backend/fetch_response.py
+++ backend/fetch_response.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from llama_index.core import PromptTemplate
-
+from typing import List, Dict 
 client = OpenAI()
 
 def get_answer_for_query(query:str, context:str):
@@ -58,4 +58,36 @@ def get_answer_for_query(query:str, context:str):
             yield chunk.choices[0].delta.content
 
 
+def transform_query(query:str, older_conversation: List[Dict[str,str]]):
+    """
+    transforming the query to make it more clear and concise
+    Example:
+    query = 'Who was songtsen gampo?'
+    llm response = 'Songtsen gampo was a Tibetan Kings,.....'
+    
+    query 2 = 'who were his wife?' (After query transformation = 'Who were the wives of Songtsen Gampo?') 
+    """
+    
+    template = f"""
+        
+       
+        Question: {query}
+        Transform the query such that the question is clear and concise.
+    """
+    
+    qa_template = PromptTemplate(template)
+    prompt = qa_template.format(question=query)
+    
+    """ include the older conversation to make the model understand the context of the query transformation"""
+    messages = older_conversation
+    messages.extend([{"role": "user", "content": prompt}])
+
+    response = client.chat.completions.create(
+         model="gpt-4-turbo",
+        messages=messages,
+        temperature=0.3,
+        
+    )
+    transformed_query = response.choices[0].message.content
+    return transformed_query
     

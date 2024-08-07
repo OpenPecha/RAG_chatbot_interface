d4a42abfa7a4042ab5f69a7782a3a6a9c80fe4d5
tenzin3 <tenzintsunduebhattu@gmail.com>
2024-08-07 15:06:57 +0530

    show an alert message when the token limit reaches

--- frontend/frontend_main.py
+++ frontend/frontend_main.py
@@ -4,9 +4,7 @@ from typing import List, Dict
 
 from log_response import log_rag_chatbot_response
 
-
-CHATGPT_TOKEN_LIMIT = 1000
-
+CHATGPT_TOKEN_LIMIT = 5000
 
 def get_token_used():
     token_used = 0
@@ -18,16 +16,18 @@ st.set_page_config(page_title="RAG chatbot")
 with st.sidebar:
     st.title('RAG Chatbot')
 
+# Initialize token_used in session state
+if "token_used" not in st.session_state:
+    st.session_state.token_used = 0
+
 with st.container():
     # Token usage progress bar
-    
     if "messages" not in st.session_state:
-        token_used = 0
-
+        st.session_state.token_used = 0
     else:
-        token_used = get_token_used()
+        st.session_state.token_used = get_token_used()
 
-    progress = int((token_used/CHATGPT_TOKEN_LIMIT) * 100)
+    progress = int((st.session_state.token_used/CHATGPT_TOKEN_LIMIT) * 100)
 
     st.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
     # Create columns for side-by-side layout
@@ -36,6 +36,7 @@ with st.container():
         st.write("**Token usage**")
 
     with col2:
+        progress = progress if progress <= 100 else 100
         progress_bar = st.progress(progress)
 
 st.markdown(
@@ -65,30 +66,34 @@ for message in st.session_state.messages:
 
 # Function to get response from the backend
 def get_response_from_backend(user_input: str, older_conversation: List[Dict]):
-    for chunk in requests.post("http://127.0.0.1:8000", json={"user_input": user_input, "older_conversation":older_conversation}, stream=True):
+    for chunk in requests.post("http://127.0.0.1:8000", json={"user_input": user_input, "older_conversation": older_conversation}, stream=True):
         yield chunk.decode("utf-8") 
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+if st.session_state.token_used < CHATGPT_TOKEN_LIMIT:
+    # React to user input
+    if prompt := st.chat_input("What is up?", disabled=st.session_state.token_used >= CHATGPT_TOKEN_LIMIT):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
+        full_response = ""
 
-    full_response = ""
+        with st.chat_message("assistant"):
+            response = get_response_from_backend(prompt, st.session_state.messages)
+            full_response = st.write_stream(response)
 
-    with st.chat_message("assistant"):
-        response = get_response_from_backend(prompt, st.session_state.messages)
-        full_response = st.write_stream(response)
+        log_rag_chatbot_response(prompt, full_response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
 
-    log_rag_chatbot_response(prompt, full_response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        # Update token_used in session state
+        st.session_state.token_used = get_token_used()
 
-    token_used = get_token_used()
+        progress = int((st.session_state.token_used/CHATGPT_TOKEN_LIMIT) * 100)
+        progress = progress if progress <= 100 else 100
+        progress_bar.progress(progress)
 
-    progress = int((token_used/CHATGPT_TOKEN_LIMIT) * 100)
-    progress_bar.progress(progress)
-
-
+else:
+    st.warning("__You have reached the token limit. Please reload the page to continue using.__")
+    st.stop()
